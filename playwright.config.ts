736277185c76bc97120b93516874d90e0b88ba52
rocrisp <rocrisp@redhat.com>
2025-02-18 09:58:01 -0500

    debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -52,88 +52,8 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: "showcase",
-      ...useCommonDeviceAndViewportConfig,
-      testIgnore: [
-        "**/playwright/e2e/plugins/rbac/**/*.spec.ts",
-        "**/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts",
-        "**/playwright/e2e/verify-tls-config-with-external-postgres-db.spec.ts",
-        "**/playwright/e2e/authProviders/**/*.spec.ts",
-        "**/playwright/e2e/plugins/bulk-import.spec.ts",
-        "**/playwright/e2e/verify-tls-config-health-check.spec.ts",
-        "**/playwright/e2e/configuration-test/config-map.spec.ts",
-      ],
-    },
-    {
-      name: "showcase-rbac",
-      ...useCommonDeviceAndViewportConfig,
-      testMatch: [
-        "**/playwright/e2e/plugins/rbac/**/*.spec.ts",
-        "**/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts",
-        "**/playwright/e2e/verify-tls-config-with-external-postgres-db.spec.ts",
-        "**/playwright/e2e/plugins/bulk-import.spec.ts",
-      ],
-    },
-    {
-      name: "showcase-auth-providers",
-      use: {
-        ...devices["Desktop Chrome"],
-        viewport: { width: 1920, height: 1080 },
-      },
-      testMatch: ["**/playwright/e2e/authProviders/*.spec.ts"],
-      testIgnore: [
-        "**/playwright/e2e/authProviders/setup-environment.spec.ts",
-        "**/playwright/e2e/authProviders/clear-environment.spec.ts",
-        "**/playwright/e2e/verify-tls-config-health-check.spec.ts",
-      ],
-      dependencies: ["showcase-auth-providers-setup-environment"],
-      teardown: "showcase-auth-providers-clear-environment",
-      retries: 2,
-    },
-    {
       name: "smoke-test",
       testMatch: ["**/playwright/e2e/smoke-test.ts"],
-    },
-    {
-      name: "showcase-auth-providers-clear-environment",
-      testMatch: ["**/playwright/e2e/authProviders/clear-environment.spec.ts"],
-    },
-    {
-      name: "showcase-k8s",
-      ...useCommonDeviceAndViewportConfig,
-      testIgnore: [
-        "**/playwright/e2e/plugins/rbac/**/*.spec.ts",
-        "**/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts",
-        "**/playwright/e2e/verify-tls-config-with-external-postgres-db.spec.ts",
-        "**/playwright/e2e/authProviders/**/*.spec.ts",
-        "**/playwright/e2e/plugins/bulk-import.spec.ts",
-        "**/playwright/e2e/plugins/tekton/tekton.spec.ts",
-        "**/playwright/e2e/catalog-scaffoldedfromLink.spec.ts",
-        "**/playwright/e2e/plugins/ocm.spec.ts",
-        "**/playwright/e2e/audit-log/**/*.spec.ts",
-        "**/playwright/e2e/verify-redis-cache.spec.ts",
-        "**/playwright/e2e/plugins/topology/topology.spec.ts",
-        "**/playwright/e2e/verify-tls-config-health-check.spec.ts",
-      ],
-    },
-    {
-      name: "showcase-rbac-k8s",
-      ...useCommonDeviceAndViewportConfig,
-      testMatch: [
-        "**/playwright/e2e/plugins/rbac/**/*.spec.ts",
-        "**/playwright/e2e/plugins/analytics/analytics-disabled-rbac.spec.ts",
-        "**/playwright/e2e/plugins/bulk-import.spec.ts",
-      ],
-    },
-    {
-      name: "postgres-health-check",
-      ...useCommonDeviceAndViewportConfig,
-      testMatch: ["**/playwright/e2e/verify-tls-config-health-check.spec.ts"],
-    },
-    {
-      name: "showcase-runtime",
-      ...useCommonDeviceAndViewportConfig,
-      testMatch: ["**/playwright/e2e/configuration-test/config-map.spec.ts"],
     },
 
     // {
